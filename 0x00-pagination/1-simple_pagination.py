d35717489ee757101b5be4eac7ebbe21c47802d1
Elijahdanie <elijahdanie@gmail.com>
2022-05-04 12:58:46 +0100

    errors fixed

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -48,6 +48,6 @@ def index_range(page: int, page_size: int) -> Tuple:
     start and end indexes used to access
     a list of data
     """
-    start = page - 1 * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
